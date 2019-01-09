f29e835ef4af3841f99114cfcbc7897788b53c69
YingRui Lu <yingruilu@zhulux.com>
2019-01-09 14:10:05 +0800

    support gen wavefile url on s3 for morse code

--- morseCode/app/functions/encoder.rb
+++ morseCode/app/functions/encoder.rb
@@ -1,9 +1,18 @@
 require 'json'
+require 'aws-sdk'
 require 'morse_code'
 
 def handler(event:, context:)
   params = event['queryStringParameters'] || Hash.new('')
-  content = MorseCode::Encoder.new(params['content']).encode
-  body = { content: content, original: params['content'] }
+  encoder = MorseCode::Encoder.new(params['content'])
+  content = encoder.encode
+  wavefile = encoder.wave
+
+  s3 = Aws::S3::Resource.new(region: 'us-west-1')
+  bucket = s3.bucket(ENV['BUCKET'])
+  object = bucket.object(wavefile.split('/').last)
+  object.upload_file(wavefile)
+
+  body = { content: content, original: params['content'], wav_url: object.presigned_url(:get, expires_in: 1800) }
   { statusCode: 200, body: JSON.generate(body) }
 end
