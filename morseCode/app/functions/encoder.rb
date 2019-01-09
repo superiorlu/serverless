9dfd3cb2f394800b3625c2e1f602d9a26063a736
YingRui Lu <yingruilu@zhulux.com>
2019-01-09 15:08:13 +0800

    fix content empty

--- morseCode/app/functions/encoder.rb
+++ morseCode/app/functions/encoder.rb
@@ -4,15 +4,17 @@ require 'morse_code'
 
 def handler(event:, context:)
   params = event['queryStringParameters'] || Hash.new('')
-  encoder = MorseCode::Encoder.new(params['content'])
-  content = encoder.encode
-  wavefile = encoder.wave
-
-  s3 = Aws::S3::Resource.new(region: 'us-west-1')
-  bucket = s3.bucket(ENV['BUCKET'])
-  object = bucket.object(wavefile.split('/').last)
-  object.upload_file(wavefile)
-
-  body = { content: content, original: params['content'], wav_url: object.presigned_url(:get, expires_in: 1800) }
+  body = if params['content'].empty?
+           { content: '', original: '' }
+         else
+           encoder = MorseCode::Encoder.new(params['content'])
+           content = encoder.encode
+           wavefile = encoder.wave
+           s3 = Aws::S3::Resource.new(region: 'us-west-1')
+           bucket = s3.bucket(ENV['BUCKET'])
+           object = bucket.object(wavefile.split('/').last)
+           object.upload_file(wavefile)
+           { content: content, original: params['content'], wav_url: object.presigned_url(:get, expires_in: 1800) }
+        end
   { statusCode: 200, body: JSON.generate(body) }
 end
